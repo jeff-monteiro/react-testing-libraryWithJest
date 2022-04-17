275911b892ef6f9e555ca4bb55ffe75e9698496e
Jefferson <jeffmonteiro61@icloud.com>
2022-04-17 18:18:52 -0300

    initialConditionsSummaryForm

---
+++ sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -0,0 +1,13 @@
+import {render, screen, fireEvent} from '@testing-library/react';
+import SummaryForm from '../SummaryForm';
+
+test('Initial conditions', () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole('checkbox', {
+    name: /terms and conditions/i,
+  });
+  expect(checkbox).not.toBeChecked();
+
+  const confirmButton = screen.getByRole('button', { name: /confirm order/i });
+  expect(confirmButton).toBeDisabled();
+})
