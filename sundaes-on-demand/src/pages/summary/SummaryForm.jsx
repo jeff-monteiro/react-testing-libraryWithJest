3138dc9ffcbe2ef1d9fe1db3f7bad353532c2ac7
Jefferson <jefferson.moura@hp.com>
2022-04-18 18:23:57 -0300

    testImplementationSummaryForm

--- sundaes-on-demand/src/pages/summary/SummaryForm.jsx
+++ sundaes-on-demand/src/pages/summary/SummaryForm.jsx
@@ -1,3 +1,15 @@
+import React, {useState} from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 export default function SummaryForm() {
+
+  const [tcChecked, setTcChecked] = useState(false);
+
+  const checkboxLabel = (
+    <span>
+      I agree to <span style={{color: 'blue'}}>Terms and Conditions</span>
+    </span>
+  )
   return <div />
 }
