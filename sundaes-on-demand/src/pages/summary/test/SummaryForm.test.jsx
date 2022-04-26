86294694c38f7fa93234abc615fb934e935f1be9
Jefferson <jeffmonteiro61@icloud.com>
2022-04-25 23:28:18 -0300

    awaitFunction

--- sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,4 +1,4 @@
-import {render, screen} from '@testing-library/react';
+import {render, screen, waitForElementToBeRemoved} from '@testing-library/react';
 import SummaryForm from '../SummaryForm';
 import userEvent from '@testing-library/user-event'
 
@@ -26,7 +26,7 @@ test('Checkbox enables button on first click and disables on secondclick', () =>
   expect(confirmButton).toBeDisabled();
 });
 
-test('popover responds to hover', () => {
+test('popover responds to hover', async() => {
   render(<SummaryForm />);
   // popover starts out hidden
   const nullPopover = screen.queryByText(/no ice cream will actually be delivered/i);
@@ -39,7 +39,7 @@ test('popover responds to hover', () => {
   expect(popover).toBeInTheDocument();
   // popover disappears when we mouse out
   userEvent.unhover(termsAndConditions);
-  const nullPopoverAgain = screen.queryByText(/no ice cream will actually be delivered/i);
-  expect(nullPopoverAgain).not.toBeInTheDocument();
+  await waitForElementToBeRemoved(() => screen.queryByText(/no ice cream will actually be delivered/i));
+  
 
 });
