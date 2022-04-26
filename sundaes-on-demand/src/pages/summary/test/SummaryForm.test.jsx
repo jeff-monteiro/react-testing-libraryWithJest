d78cab3de79bc11bdcb327cd121565be5560e10d
Jefferson <jeffmonteiro61@icloud.com>
2022-04-26 15:51:25 -0300

    awaitElementToBeRemoved

--- sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -28,15 +28,18 @@ test('Checkbox enables button on first click and disables on secondclick', () =>
 
 test('popover responds to hover', async() => {
   render(<SummaryForm />);
+  
   // popover starts out hidden
   const nullPopover = screen.queryByText(/no ice cream will actually be delivered/i);
   expect(nullPopover).not.toBeInTheDocument();
+
   // popover appears upon mouseover of checkbox label
   const termsAndConditions = screen.getByText(/terms and conditions/i);
   userEvent.hover(termsAndConditions);
 
   const popover = screen.getByText(/no ice cream will actually be delivered/i);
   expect(popover).toBeInTheDocument();
+
   // popover disappears when we mouse out
   userEvent.unhover(termsAndConditions);
   await waitForElementToBeRemoved(() => screen.queryByText(/no ice cream will actually be delivered/i));
